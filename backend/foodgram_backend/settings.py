e0a0413d590d3ea309490d244edd69d15e892fe0
Никита Васин <cskovec22@yandex.com>
2024-03-30 22:54:20 +0700

    После первого ревью

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -60,26 +60,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "foodgram_backend.wsgi.application"
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv(
-#             "POSTGRES_ENGINE",
-#             "django.db.backends.postgresql"
-#         ),
-#         "NAME": os.getenv("POSTGRES_DB", "postgres"),
-#         "USER": os.getenv("POSTGRES_USER", "postgres"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-#         "HOST": os.getenv("DB_HOST", "db"),
-#         "PORT": os.getenv("DB_PORT", 5432)
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3"
+        "ENGINE": os.getenv(
+            "POSTGRES_ENGINE",
+            "django.db.backends.postgresql"
+        ),
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", 5432)
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3"
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
